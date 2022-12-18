f5621790eaccfb841cf4e6bc66328e37877ddbd4
MracusRin <aleksandrovi44@gmail.com>
2022-12-18 21:26:35 +0500

    init commit

---
+++ src/test/java/com/mracus/RESTWeatherApp/RestWeatherAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mracus.RESTWeatherApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWeatherAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
